32d173e2b4ad88667b1f72b6f518ed8bd5e1eb02
YuselmiRamirez <142958554+YuselmiRamirez@users.noreply.github.com>
2025-11-12 00:56:07 -0600

    Práctica 4_1.c

--- Práctica 4/Práctica 4_1.c
+++ Práctica 4/Práctica 4_1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int n;
+
+    printf("Bienvenido.\n");
+    printf("Â¿Cuantos enteros desea guardar?: ");
+    scanf("%d", &n);
+
+    int *arr=(int*)malloc(n*sizeof(int));
+
+    printf("\nValores en la memoria:\n");
+    for (int i=0; i<n; i++){
+        printf("%d", arr[i]);
+        printf("\n");
+    }
+    free(arr);
+
+    return 0;
+}
